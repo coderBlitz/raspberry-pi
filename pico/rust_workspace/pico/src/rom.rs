368f88633c8924a47bb0c2043b9df39c599e934e
Christopher Skane <sokrbball@gmail.com>
2024-11-20 23:29:43 -0500

    Fixed the ROM lookup function to use correct integer size for fetching pointer values.

--- pico/rust_workspace/pico/src/rom.rs
+++ pico/rust_workspace/pico/src/rom.rs
@@ -1,8 +1,4 @@
-use core::ffi;
 use crate::consts;
-
-const FUNC_TABLE: *const u16 = consts::ROM_FUNC_TABLE as _;
-const DATA_TABLE: *const u16 = consts::ROM_DATA_TABLE as _;
 
 /// ROM function table lookup function.
 ///
@@ -10,28 +6,31 @@ const DATA_TABLE: *const u16 = consts::ROM_DATA_TABLE as _;
 type RomTableLookupFn<T> = unsafe extern "C" fn(table: *const u16, code: u32) -> T;
 
 /// Convert lookup characters to code for a [RomTableLookupFn].
-fn rom_table_code(c1: u8, c2: u8) -> u32 {
+#[inline(always)]
+pub fn rom_table_code(c1: u8, c2: u8) -> u32 {
 	(c2 as u32) << 8 | c1 as u32
 }
 
+#[inline(always)]
 fn get_lookup_fn<T>() -> RomTableLookupFn<T> {
 	unsafe {
-		let lookup_fn = core::mem::transmute(consts::ROM_TABLE_LOOKUP_FN);
-		lookup_fn
+		let addr = *(consts::ROM_TABLE_LOOKUP_FN as *const u16) as u32;
+		core::mem::transmute(addr)
 	}
 }
 
-fn rom_table_lookup<T>(table: *const u16, code: u32) -> T {
+pub fn func_table_lookup<T>(code: u32) -> T {
+	let func_table: *const u16 = unsafe { *(consts::ROM_FUNC_TABLE as *const u16) as *const u16};
 	let lookup = get_lookup_fn::<T>();
 
 	unsafe {
-		lookup(table, code)
+		lookup(func_table, code)
 	}
 }
 
-// TODO: Debug
+// TODO: Try again before more debugging. Fixed get_lookup_fn and fun_table_lookup.
 pub fn flash_enter_cmd_xip() {
 	let code = rom_table_code(b'C', b'X');
-	let enter_xip: extern "C" fn() = rom_table_lookup(FUNC_TABLE, code);
+	let enter_xip: extern "C" fn() = func_table_lookup(code);
 	enter_xip();
 }
