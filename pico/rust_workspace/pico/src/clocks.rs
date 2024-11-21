5a49de9399c9f51ddafcdb6e6e56928f45c0a417
Christopher Skane <sokrbball@gmail.com>
2024-11-20 22:10:43 -0500

    Added functions to setup PLL and change SYS clock to PLL.

--- pico/rust_workspace/pico/src/clocks.rs
+++ pico/rust_workspace/pico/src/clocks.rs
@@ -5,6 +5,11 @@ static XOSC_CTRL: Register = unsafe { Register::new(all::XOSC_CTRL) };
 static XOSC_STATUS: Register = unsafe { Register::new(all::XOSC_STATUS) };
 static XOSC_STARTUP: Register = unsafe { Register::new(all::XOSC_STARTUP) };
 
+static PLL_SYS_CS: Register = unsafe { Register::new(all::PLL_SYS_CS) };
+static PLL_SYS_PWR: Register = unsafe { Register::new(all::PLL_SYS_PWR) };
+static PLL_SYS_FBDIV_INT: Register = unsafe { Register::new(all::PLL_SYS_FBDIV_INT) };
+static PLL_SYS_PRIM: Register = unsafe { Register::new(all::PLL_SYS_PRIM) };
+
 static CLK_REF_CTRL: Register = unsafe { Register::new(all::CLOCKS_CLK_REF_CTRL) };
 static CLK_REF_SELECTED: Register = unsafe { Register::new(all::CLOCKS_CLK_REF_SELECTED) };
 
@@ -30,5 +35,34 @@ pub fn ref_to_xosc() {
 	CLK_REF_CTRL.set(CLOCKS_CLK_REF_CTRL_SRC_XOSC_CLKSRC);
 
 	// Wait for the switch
+	while CLK_REF_SELECTED.get() == 0 {}
+}
+
+pub fn configure_pll() {
+	// FBDIV 133
+	// POSTDIV1 6 x POSTDIV2 2 = 12 POSTDIV
+
+	// Power on PLL system
+	crate::resets::enable_pll_sys();
+
+	// Enable all PLL PD registers
+	PLL_SYS_PWR.atomic_bitclear(0xFF);
+
+	// Set FBDIV
+	PLL_SYS_FBDIV_INT.set(133);
+
+	// Set POSTDIVs
+	PLL_SYS_PRIM.set(6 << all::PLL_SYS_PRIM_POSTDIV1_SHIFT | 2 << all::PLL_SYS_PRIM_POSTDIV2_SHIFT);
+}
+
+pub fn sys_to_pll() {
+	// Set SYS AUX to PLL
+	CLK_SYS_CTRL.atomic_bitclear(all::CLOCKS_CLK_SYS_CTRL_AUXSRC_BITS);
+	CLK_SYS_CTRL.atomic_bitset(all::CLOCKS_CLK_SYS_CTRL_AUXSRC_CLKSRC_PLL_SYS);
+
+	// Set the glitchless to the AUX
+	CLK_SYS_CTRL.atomic_bitset(all::CLOCKS_CLK_SYS_CTRL_SRC_BIT);
+
+	// Wait for switch
 	while CLK_REF_SELECTED.get() == 0 {}
 }
