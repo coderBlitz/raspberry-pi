4554920d7fe9fb96e5ee1037b7eb68bb865c2849
Christopher Skane <sokrbball@gmail.com>
2024-12-03 21:26:42 -0500

    Added remaining functions for CTRL register.

--- pico/rust_workspace/pico/src/gpio.rs
+++ pico/rust_workspace/pico/src/gpio.rs
@@ -2,6 +2,15 @@ use crate::consts::{IO_BANK0_BASE, IO_QSPI_BASE};
 use crate::consts::gpio::*;
 use crate::registers::Register;
 
+#[derive(Clone, Copy, Eq, PartialEq)]
+#[repr(u32)]
+pub enum GpioOverride {
+	Normal = GPIO_CTRL_NORMAL,
+	Invert = GPIO_CTRL_INVERT,
+	LowDisable = GPIO_CTRL_LOWDIS,
+	HighEnable = GPIO_CTRL_HIGHEN,
+}
+
 /// Represents a GPIO pin.
 // Stored value is the address of the STATUS register, CTRL is +4. Pad control
 //  is PADS_BANK0_BASE + id*4.
@@ -45,5 +54,26 @@ impl Gpio {
 	pub fn use_f5(&self) {
 		self.0.atomic_bitclear(IO_BANK0_GPIO_CTRL_FUNCSEL_BITS);
 		self.0.atomic_bitset(GPIO_CTRL_F5);
+	}
+
+	/// Set interrupt override.
+	pub fn irq_override(&self, v: GpioOverride) {
+		self.0.atomic_bitclear(IO_BANK0_GPIO_CTRL_IRQOVER_BITS);
+		self.0.atomic_bitset((v as u32) << IO_BANK0_GPIO_CTRL_IRQOVER_SHIFT);
+	}
+	/// Set input override.
+	pub fn in_override(&self, v: GpioOverride) {
+		self.0.atomic_bitclear(IO_BANK0_GPIO_CTRL_INOVER_BITS);
+		self.0.atomic_bitset((v as u32) << IO_BANK0_GPIO_CTRL_INOVER_SHIFT);
+	}
+	/// Set output enable override.
+	pub fn oe_override(&self, v: GpioOverride) {
+		self.0.atomic_bitclear(IO_BANK0_GPIO_CTRL_OEOVER_BITS);
+		self.0.atomic_bitset((v as u32) << IO_BANK0_GPIO_CTRL_OEOVER_SHIFT);
+	}
+	/// Set output override.
+	pub fn out_override(&self, v: GpioOverride) {
+		self.0.atomic_bitclear(IO_BANK0_GPIO_CTRL_OUTOVER_BITS);
+		self.0.atomic_bitset((v as u32) << IO_BANK0_GPIO_CTRL_OUTOVER_SHIFT);
 	}
 }
